f2f99b5ee319565047d534fb4f028270a6a5b259
Yuri <yuricda96@gmail.com>
2020-06-23 01:03:29 -0300

    feat( Create Api models and folder structure)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TouristSpotApi
+
+#
---
+++ attractions/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Attraction(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    opening_hours = models.TextField()
+    minor_age = models.IntegerField()
+
+    def __str__(self):
+        return  self.name
