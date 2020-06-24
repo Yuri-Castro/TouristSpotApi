846154c7e005906d2c98e2525ff5cf1827b9755d
Yuri <yuricda96@gmail.com>
2020-06-23 23:51:11 -0300

    address models

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from attractions.models import  Attraction
+from attractions.models import Attraction
+from comments.models import Comment
+from address.models import Address
 
 
 class TouristSpot(models.Model):
@@ -7,6 +9,9 @@ class TouristSpot(models.Model):
     description = models.TextField()
     approved = models.BooleanField(default=False)
     attractions = models.ManyToManyField(Attraction)
+    comments = models.ManyToManyField(Comment)
+    address = models.ForeignKey(Address)
 
     def __str__(self):
         return self.name
+
--- touristspot/settings.py
+++ touristspot/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
-    'attractions'
+    'attractions',
+    'comments',
+    'reviews',
+    'address'
 ]
 
 MIDDLEWARE = [
